830beb85f37b9a8033ea2900e66a8b716d60b72d
tosha5252 <tosha5252@yahoo.com>
2021-04-18 20:28:18 -0500

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package week2;
+
+import acm.program.*;
+
+public class Fibonacci extends ConsoleProgram {
+	
+	public static void main(String[] args) {
+
+        int n = 10000;
+        int x = 0;
+        int y = 1;
+        
+        System.out.println("This program lists the Fibonacci sequence.");
+        while (x < n)
+        {   
+        	
+            System.out.print(x + "\n");
+
+            int fib = x + y;
+            x = y;
+            y = fib;
+        }
+    }
+}
